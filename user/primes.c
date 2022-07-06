ac0c86b39ba07c0d6265182f6b5584ea111edd8b
LuCC-que <chen1968493689@gmail.com>
2022-07-05 20:26:05 -0400

    recall the function after fork, so the child can start over again, tryit one more time tm

--- user/primes.c
+++ user/primes.c
@@ -3,13 +3,30 @@
 #include "user/user.h"
 #define MAX 35;
 #define CUR 2;
+
+void fill_array(int *array)
+{
+  for (int i = 0; i < 35; ++i)
+  {
+    array[i] = i + 1;
+  }
+}
+
 int main(void)
 {
 
   printf("primes lab");
 
-  for (int i = 2; i <= MAX; ++i)
+  // int pip[2];
+  int array[35];
+  fill_array(array);
+
+  int pip[2];
+  int pip[2];
+  if (pipe(pip) == -1)
   {
+    printf("fail to create pip\n");
+    exit(1);
   }
 
   exit(0);
