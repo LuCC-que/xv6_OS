8a4ac5f99e5d0c23c09d2ac68c76507375ea7ab9
LuCC-que <chen1968493689@gmail.com>
2022-07-12 16:18:12 -0400

    prime done

--- user/primes.c
+++ user/primes.c
@@ -1,32 +1,74 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#define MAX 35;
-#define CUR 2;
 
-void fill_array(int *array)
+void primes(int *fd)
 {
-  for (int i = 0; i < 35; ++i)
+  int p, d;
+  close(fd[1]);
+  // read one value
+  if (read(fd[0], (void *)&p, sizeof(p)) != sizeof(p))
   {
-    array[i] = i + 1;
+    fprintf(2, "Read fail!\n");
+    exit(1);
   }
+  // print it out
+  printf("prime %d\n", p);
+
+  if (read(fd[0], (void *)&d, sizeof(d)))
+  {
+    int fd1[2];
+    pipe(fd1);
+    if (fork() == 0)
+    {
+      // child call the function again
+      primes(fd1);
+    }
+    else
+    {
+      close(fd1[0]);
+      do
+      {
+        if (d % p != 0)
+        {
+          write(fd1[1], (void *)&d, sizeof(d));
+        }
+      } while (read(fd[0], (void *)&d, sizeof(d)));
+
+      // close file desciptor, save resource for child
+      close(fd[0]);
+      close(fd1[1]);
+      wait(0);
+    }
+  }
+  exit(0);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-
-  printf("primes lab");
-
-  // int pip[2];
-  int array[35];
-  fill_array(array);
-
-  int pip[2];
-  int pip[2];
-  if (pipe(pip) == -1)
+  int fd[2];
+  int start = 2;
+  int end = 35;
+  pipe(fd);
+  if (fork() == 0)
   {
-    printf("fail to create pip\n");
-    exit(1);
+    // child
+    primes(fd);
+  }
+  else
+  {
+    close(fd[0]);
+    for (int i = start; i <= end; i++)
+    {
+      // file descriptors are not neccassarily files.
+      if (write(fd[1], (void *)&i, sizeof(i)) != 4)
+      {
+        fprintf(2, "Write fail!\n");
+        exit(1);
+      }
+    }
+    close(fd[1]);
+    wait(0);
   }
 
   exit(0);
